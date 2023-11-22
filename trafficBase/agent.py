091f7845b95ef08ba1383066f30eaac0f24a3321
A01748013 <dulce.garcia.r.03@gmail.com>
2023-11-22 14:57:50 -0600

    Se mueve avoiding los obstacles, con AStar

--- trafficBase/agent.py
+++ trafficBase/agent.py
@@ -1,32 +1,97 @@
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 from mesa import Agent
+import random
 
 class Car(Agent):
-    """
-    Agent that moves randomly.
-    Attributes:
-        unique_id: Agent's ID 
-        direction: Randomly chosen direction chosen from one of eight directions
-    """
     def __init__(self, unique_id, model):
-        """
-        Creates a new random agent.
-        Args:
-            unique_id: The agent's ID
-            model: Model reference for the agent
-        """
         super().__init__(unique_id, model)
+        self.destination = self.choose_random_destination()  
+
 
     def move(self):
-        """ 
-        Determines if the agent can move in the direction that was chosen
-        """        
-        self.model.grid.move_to_empty(self)
+        
+
+        # Convertir la cuadrícula de Mesa a una cuadrícula para A*
+        grid_matrix = []
+        for y in range(self.model.grid.height):
+            row = []
+            for x in range(self.model.grid.width):
+                cell_contents = self.model.grid.get_cell_list_contents((x, y))
+                obstacle = any(isinstance(agent, Obstacle) for agent in cell_contents)
+                row.append(0 if obstacle else 1)
+            grid_matrix.append(row)
+
+    
+        grid = Grid(matrix=grid_matrix)  # Crear el objeto Grid
+
+        # print("Matriz del Grid:")
+        # for row in grid_matrix:
+        #     print(' '.join(str(cell) for cell in row))
+
+
+        # Iniciar y destino fijo
+        start = grid.node(*self.pos)
+        end = grid.node(self.destination[0],self.destination[1])
+
+
+        # print("Coche esta en:")
+        # print(self.pos[1])
+        # print(self.pos[0])
+        # print("Destino esta en:")
+        # print(self.destination[1])
+        # print(self.destination[0])
+
+
+         # Asegúrate de que tanto start como end no estén en obstáculos
+        # if grid_matrix[self.pos[1]][self.pos[0]] == 0:
+        #     print("El inicio está en un obstáculo")
+        # if grid_matrix[self.destination[1]][self.destination[0]] == 0:
+        #     print("El destino está en un obstáculo")
+        #     return
+
+
+
+        # A* Finder
+        finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+        path, _ = finder.find_path(start, end, grid)
+ 
+        for node in path:
+            print(f"{node.x} , {node.y}", end=" - ")
+
+
+        # Imprimir la ruta generada
+        print("Ruta generada por A*:", path)
+        print(grid.grid_str(path=path, start=start, end=end))
+
+
+        # Mover a la siguiente posición en el camino
+        if path and len(path) > 1:
+            next_position = path[1]  # El segundo elemento es el siguiente paso
+            self.model.grid.move_agent(self, next_position)
+
+
+
+
+    def choose_random_destination(self):
+        destinations = [agent.pos for agent in self.model.schedule.agents 
+                        if isinstance(agent, Destination)]
+        # print("Destinos disponibles:", destinations)  # Para depuración
+        chosen_destination = random.choice(destinations) if destinations else None
+        print("Destino elegido:", chosen_destination)  # Nueva línea de depuración
+        return chosen_destination
+
+
 
     def step(self):
-        """ 
-        Determines the new direction it will take, and then moves
-        """
         self.move()
+        if self.pos == self.destination:
+            self.model.running = False  # Detiene la simulación
+
+
+
+            
 
 class Traffic_Light(Agent):
     """
